dc0748a5f7d6f980743434f31a40d11b57ed3269
abrasumente <345396594@qq.com>
2022-03-02 21:17:51 +0800

    add neon summing routine with 4x unroll

--- sum.cpp
+++ sum.cpp
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 #include <stdlib.h>
+#include <arm_neon.h>
 
 #define no_auto_vectorize                                                      \
     _Pragma("clang loop vectorize(disable) interleave(disable)")
@@ -12,8 +13,23 @@ static int *prepare_array(int N) {
     for (int i = 0; i < N; i++) {
         arr[i] = i+1;
     }
-
     return arr;
+}
+
+int sum_neon_4x(int *arr, int N) {
+    constexpr int M = 4; // unroll factor
+    int res = 0;
+
+    int i;
+    no_auto_vectorize for (i = 0; i < N; i += M, arr += 4) {
+        int32x4_t four_ints = vld1q_s32(arr);
+        int32_t acc = vaddvq_s32(four_ints);
+        res += acc;
+    }
+
+    no_auto_vectorize for (i -= M; i < N; i++) { res += arr[i]; }
+
+    return res;
 }
 
 #define SUM_UNROLL_IMPL(UNROLL_FACTOR)                                         \
@@ -68,11 +84,20 @@ int sum_auto_vec(int *arr, int N) {
     }                                                                          \
     BENCHMARK(bench_##NAME)->Arg(10)->Arg(1000)->Arg(10000)->Arg(1000000)
 
+BENCH_SUM(neon_4x);
 BENCH_SUM(unroll_4x);
-BENCH_SUM(unroll_8x);
-BENCH_SUM(unroll_16x);
-BENCH_SUM(unroll_32x);
-BENCH_SUM(naive);
+//BENCH_SUM(unroll_8x);
+//BENCH_SUM(unroll_16x);
+//BENCH_SUM(unroll_32x);
+//BENCH_SUM(naive);
 //BENCH_SUM(auto_vec);
 
 BENCHMARK_MAIN();
+
+/*
+int main() {
+    int *arr = prepare_array(1000);
+    int res = sum_neno_4x(arr, 1000);
+    printf("%d\n", res);
+}
+*/
